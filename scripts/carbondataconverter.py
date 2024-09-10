387054a5cb532326ed67aa26d32ca0d587920f89
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2024-09-10 10:01:19 +0200

    Refactor carbondataconverter.py to improve data enrichment and MongoDB insertion

--- scripts/carbondataconverter.py
+++ scripts/carbondataconverter.py
@@ -17,33 +17,19 @@ def enrich_with_group_info_and_insert_to_mongodb(csv_file_path, db_name, collect
     current_group_info = {}
 
     for index, row in df.iterrows():
-        identifier = row.iloc[0]  # first column is used for identification
+        identifier = row.iloc[1]  # 2nd column is used for identification
         
-        try:
-            # tries to convert the identifier to an integer, useful for checking if it's an integer
-            int_identifier = int(identifier)
-            is_integer = True
-        except ValueError:
-            is_integer = False
+        # Convert the row to a dictionary, filtering out NaN values
+        member_data = {k: v for k, v in row.dropna().to_dict().items()}
+        # Update the member data with the current group information
+        # Insert the enriched document into MongoDB
+        collection.insert_one(member_data)
 
-        # If identifier is not NaN and is an integer, then it's assumed to be a group identifier
-        if pd.notna(identifier) and is_integer:
-            current_group_info = {
-                "groupNumber": str(int_identifier),
-                "groupName": row[2]  # Assuming the third column represents the group name
-            }
-        else:
-            # Convert the row to a dictionary, filtering out NaN values
-            member_data = {k: v for k, v in row.dropna().to_dict().items()}
-            # Update the member data with the current group information
-            member_data.update(current_group_info)
-            # Insert the enriched document into MongoDB
-            collection.insert_one(member_data)
 
     print(f"Data successfully inserted into {db_name}.{collection_name}")
 
-csv_file_path = r"C:\\Users\\LouisTrümpler\\Documents\\GitHub\\IfcLCA\\data\\Oekobilanzdaten_ Baubereich_Donne_ecobilans_construction_2009-1-2022_v4_0.csv"
+csv_file_path = r"...Oekobilanzdaten_ Baubereich_Donne_ecobilans_construction_2009-1-2022_v5.0_BAUMATERIALIEN_clean.csv"
 db_name = "IfcLCAdata_01"
-collection_name = "carbon_data_KBOB"
+collection_name = "KBOB_v5_0"
 
 enrich_with_group_info_and_insert_to_mongodb(csv_file_path, db_name, collection_name)
