fe44ce72a77aa9657d2985aea4eba099dfce0549
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2024-11-05 23:46:58 +0100

    prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,13 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
-export const prisma = new PrismaClient();
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_PRISMA_URL,
+  connectionTimeoutMillis: 5000,
+});
+
+const adapter = new PrismaNeon(pool);
+export const prisma = new PrismaClient({ adapter });
 
 export type { PrismaClient } from "@prisma/client";
