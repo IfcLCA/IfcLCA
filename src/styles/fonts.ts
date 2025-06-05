10b701823e18032d8953b75776941e2b382ead9d
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2025-06-05 20:16:16 +0200

    Use fontsource for Nunito Sans

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,8 +1,28 @@
-import { Nunito_Sans } from "next/font/google";
+import localFont from "next/font/local";
 
-export const nunitoSans = Nunito_Sans({
-  subsets: ["latin"],
+export const nunitoSans = localFont({
+  src: [
+    {
+      path: "../../node_modules/@fontsource/nunito-sans/files/nunito-sans-latin-400-normal.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../node_modules/@fontsource/nunito-sans/files/nunito-sans-latin-500-normal.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../node_modules/@fontsource/nunito-sans/files/nunito-sans-latin-600-normal.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../node_modules/@fontsource/nunito-sans/files/nunito-sans-latin-700-normal.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
   display: "swap",
-  weight: ["400", "500", "600", "700"],
   variable: "--font-nunito-sans",
 });
