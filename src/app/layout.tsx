084a934f256ca78133c94589e73b46a658306264
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2025-06-05 19:00:55 +0200

    Replace Vercel favicon with placeholders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,19 @@ import { nunitoSans } from "@/styles/fonts";
 export const metadata = {
   title: "IfcLCA",
   description: "LCA Analysis for Ifc Models",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      { rel: "manifest", url: "/site.webmanifest" },
+      { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#000000" },
+      { rel: "msapplication-TileImage", url: "/mstile-150x150.png" },
+    ],
+  },
 };
 
 export default function RootLayout({
