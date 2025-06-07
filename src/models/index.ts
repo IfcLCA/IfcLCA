9d6a56b69a7cbb579584338a74f5dba7c1556b9b
Louis Trümpler <78563314+louistrue@users.noreply.github.com>
2025-06-07 11:54:00 +0200

    Add EnvironmentalMaterial model

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,3 +5,4 @@ export { Project } from "./project";
 export { Upload } from "./upload";
 export { KBOBMaterial } from "./kbob";
 export { MaterialDeletion } from "./material-deletion";
+export { EnvironmentalMaterial } from "./environmental-material";
